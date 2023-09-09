7ee6eb3cbd85fd3df1b9060be1521b5a6b2ad9b6
gitone912 <akash.101@kvsroranchi.in>
2023-09-09 22:35:51 +0530

    first commit

---
+++ frontend/components/TrustedCom.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+{/* ================= Trusted Company Section ================ */}
+const TrustedCom = () => {
+    return (
+        <section >
+            <b></b>
+        </section>
+    )
+}
+
+export default TrustedCom;
